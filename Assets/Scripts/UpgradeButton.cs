0eb38e6708bb1a64c27c692e04345f6943036361
GabrielGameDev <gabrielgamedev@gmail.com>
2025-03-20 18:37:15 -0300

    Botão de upgrade

--- Assets/Scripts/UpgradeButton.cs
+++ Assets/Scripts/UpgradeButton.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public enum UpgradeType
@@ -14,10 +15,31 @@ public class UpgradeButton : MonoBehaviour
     public List<Item> resourcesCost;
     public float increaseAmount = 1.5f;
 
+    TMP_Text buttonText;
+    int level = 1;
+
+    void Start()
+    {
+        buttonText = GetComponentInChildren<TMP_Text>();
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        string resourceName = collectArea.resource.resourceName;
+        string upgradeTypeString = upgradeType == UpgradeType.Amount ? "Efficiency" : "Interval";
+        string costText = "";
+        foreach (var item in resourcesCost)
+        {
+            costText += item.amount + " " + item.resource.name + " ";
+        }
+        buttonText.text = $"{resourceName} {upgradeTypeString} Level {level}\nCost: {costText}";
+    }
     public void Upgrade()
     {
         if (LevelManager.instance.CanBuy(resourcesCost, increaseAmount))
         {
+            level++;
             switch (upgradeType)
             {
                 case UpgradeType.Amount:
@@ -27,6 +49,7 @@ public class UpgradeButton : MonoBehaviour
                     collectArea.UpgradeInterval();
                     break;
             }
+            UpdateText();
         }
     }
 
